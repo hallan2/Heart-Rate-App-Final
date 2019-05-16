f850c6d9166ae649ce94444d66b9228dcef7b017
hallan2 <47231937+hallan2@users.noreply.github.com>
2019-05-16 01:27:20 -0400

    Add files via upload

---
+++ LightController.swift
@@ -0,0 +1,30 @@
+//
+//  LightController.swift
+//  Heart_Rate_Zones
+//
+//  Created by Antonia on 5/15/19.
+//  Copyright © 2019 Antonia. All rights reserved.
+//
+
+import UIKit
+
+class LightController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Heart_Rate_Zones
+//
+//  Created by Antonia on 5/15/19.
+//  Copyright © 2019 Antonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainlabel: UILabel!
+    
+    @IBAction func startbutton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
